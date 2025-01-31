79b67525ceeed8c6772bdd0b0a5b2d6e80b3b5f3
Davi Ferreira da Silva <sildv45@gmail.com>
2025-01-31 18:36:04 -0300

    Project Created

---
+++ src/main/java/com/API_estudo/daviDev/DaviDevApplication.java
@@ -0,0 +1,13 @@
+package com.API_estudo.daviDev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaviDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaviDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/API_estudo/daviDev/DaviDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.API_estudo.daviDev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaviDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
