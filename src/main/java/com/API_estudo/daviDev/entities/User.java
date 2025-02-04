cb01ec14efb168267b222065998749d72556c122
Davi Ferreira da Silva <sildv45@gmail.com>
2025-02-04 12:03:15 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/API_estudo/daviDev/entities/User.java
+++ src/main/java/com/API_estudo/daviDev/entities/User.java
@@ -1,13 +1,15 @@
 package com.API_estudo.daviDev.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
-import jakarta.annotation.Generated;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 
@@ -23,6 +25,10 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	
 	public User() {
@@ -88,6 +94,11 @@ public class User implements Serializable {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
+
 
 
 	@Override
@@ -107,6 +118,11 @@ public class User implements Serializable {
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
+
+
+	
+
+	
 	
 	
 
