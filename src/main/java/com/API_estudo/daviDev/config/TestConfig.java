e8880966cee3c411937442c4c07a5e53074bc2df
Davi Ferreira da Silva <sildv45@gmail.com>
2025-02-04 21:18:53 -0300

    OrderStatus enum

--- src/main/java/com/API_estudo/daviDev/config/TestConfig.java
+++ src/main/java/com/API_estudo/daviDev/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.API_estudo.daviDev.entities.Order;
 import com.API_estudo.daviDev.entities.User;
+import com.API_estudo.daviDev.entities.enums.OrderStatus;
 import com.API_estudo.daviDev.repositories.OrderRepository;
 import com.API_estudo.daviDev.repositories.UserRepository;
 
@@ -33,9 +34,9 @@ public class TestConfig implements CommandLineRunner {
 		User u3 = new User(null, "Danil Raui", "dani@yahoo.com", "0022114477", "7852");
 		
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.WAITING_PAYMENT, u1);
 		
 		userRepository.saveAll(Arrays.asList(u1,u2,u3));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
